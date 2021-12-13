b7f32e79e5e84cda5f8e1444e952ec2ae92394d4
7devanle <7devanle@gmail.com>
2021-12-13 13:04:33 +0100

    change

--- src/main/java/com/ridwan/conferenceApplicationByRidwan/controllers/SessionsControllers.java
+++ src/main/java/com/ridwan/conferenceApplicationByRidwan/controllers/SessionsControllers.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/sessions")
+@RequestMapping("/api/v1/sessions")
 public class SessionsControllers {
     @Autowired
     private SessionRepositories sessionRepositories;
