bce79df82511cbfe0059bbb792907897a1fe9a4d
7devanle <7devanle@gmail.com>
2021-12-15 09:51:26 +0100

    kinf

--- src/main/java/com/ridwan/conferenceApplicationByRidwan/config/PersistenceConfiguration.java
+++ src/main/java/com/ridwan/conferenceApplicationByRidwan/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("dymond");
-        System.out.println("ARE THERE ANGELS IN HELL?");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("dymond");
+//        System.out.println("ARE THERE ANGELS IN HELL?");
+//        return builder.build();
+//    }
 }
